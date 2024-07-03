d2d68199e8f4814a5bb377fabaf8961b6d6075a0
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-07-03 20:16:19 +0800

    Logic not currently working, rework on new branch.

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-const input = [1, 5, 200, 239];
+const numbers = document.querySelectorAll('.number');
+console.log(numbers)
+const operators = document.querySelectorAll('.operator')
+console.log(operators)
+
+const display = document.querySelector('.display');
+
+let inputOne = '';
+let inputTwo = '';
+let input = [inputOne, inputTwo];
 let result;
+const operator = [add, subtract, muliply, divide];
 
 function add() {
     result = input.reduce((first, last) => first + last)
 }
-add(input);
-console.log(result);
 
 function subtract() {
     result = input.reduce((first, last) => first - last)
 }
-subtract(input);
-console.log(result);
 
 function muliply() {
     result = input.reduce((first, last) => first * last)
 }
-muliply(input);
-console.log(result);
 
 function divide() {
     result = input.reduce((first, last) => first / last)
 }
+
+add(input);
+subtract(input);
+muliply(input);
 divide(input);
-console.log(result);
+
+function opertate() {
+    display.textContent = inputOne;
+    display.textContent = inputTwo;
+    display.textContent = result;
+}
+
+numbers.forEach(num => {
+    num.addEventListener('click', updateDisplay);
+});
+
+function updateDisplay(num) {
+    let displayText = display.textContent;
+    for (let i = 0; i < numbers.length; i++) {
+        let numberText = numbers[i].innerText;
+        console.log([numberText])
+    }
+}
