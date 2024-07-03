e4b2a3c32c7125c9f79f955b75ce71e513ffd34e
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-07-03 21:14:34 +0800

    Print numbs clicked in display.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const numbers = document.querySelectorAll('.number');
-console.log(numbers);
+const operators = document.querySelectorAll('.operator');
+const display = document.querySelector('.display');
 
-let inputOne = '20';
-let inputTwo = '10';
+let inputOne = '';
+let inputTwo = '';
+let operator;
 let results;
-
 
 function add() {
     results = parseFloat(inputOne) + parseFloat(inputTwo);
@@ -22,15 +23,49 @@ function multiply() {
 }
 
 function divide() {
-    results = parseFloat(inputOne) / parseFloat(inputTwo);
+    if (parseFloat(inputTwo) !== 0) {
+        results = parseFloat(inputOne) / parseFloat(inputTwo);
+    } else {
+        results = 'ERROR'
+    }
     return results;
 }
 
-operatorAdd = add();
-console.log('Add:', operatorAdd)
-operatorSubtract = subtract();
-console.log('Subtract:', operatorSubtract);
-operatorMultiply = multiply();
-console.log('Multiply:', operatorMultiply);
-operatorDivide = divide();
-console.log('Divide:', operatorDivide);
+const operatorAdd = add();
+const operatorSubtract = subtract();
+const operatorMultiply = multiply();
+const operatorDivide = divide();
+
+numbers.forEach(num => {
+    num.addEventListener('click', getInputOne);
+    num.addEventListener('click', getInputTwo);
+});
+
+operators.forEach(op => {
+    op.addEventListener('click', getOperator)
+})
+
+function getInputOne(e) {
+    if (!operator) {
+    inputOne = e.target.innerText;
+    display.textContent += inputOne;
+    console.log(e.target.innerText);
+    }
+}
+
+function getOperator(e) {
+    operator = e.target.innerText;
+    display.textContent = '';
+    console.log(e.target.innerText)
+}
+
+function getInputTwo(e) {
+    if (operator) {
+    inputTwo = e.target.innerText;
+    display.textContent += inputTwo
+    }
+}
+
+function operate() {
+    
+}
