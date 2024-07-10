f7417508a90d60aaf7d545ea05475ab407db5c33
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-07-10 12:56:20 +0700

    Create more robust array function for operator choice.

--- script.js
+++ script.js
@@ -2,97 +2,35 @@ const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 const display = document.querySelector('.display');
 
-let inputOne = '';
-let inputTwo = '';
+let inputOne = parseFloat('');
+let inputTwo = parseFloat('');
+let arrInput = [inputOne, inputTwo,];
 let operator;
 let results;
 
 function add() {
-    results = parseFloat(inputOne) + parseFloat(inputTwo);
-    return results;
+    results = arrInput.reduce((acc, val) => acc += val)
+    console.log(results)
 }
-
 function subtract() {
-    results = parseFloat(inputOne) - parseFloat(inputTwo);
-    return results;
+    results = arrInput.reduce((acc, val) => acc -= val)
+    console.log(results)
 }
-
 function multiply() {
-    results = parseFloat(inputOne) * parseFloat(inputTwo);
-    return results;
+    results = arrInput.reduce((acc, val) => acc *= val)
+    console.log(results)
 }
-
 function divide() {
-    if (parseFloat(inputTwo) !== 0) {
-        results = parseFloat(inputOne) / parseFloat(inputTwo);
-    } else {
-        results = 'ERROR'
+    if (inputTwo !== 0) {
+    results = arrInput.reduce((acc, val) => acc /= val)
+    } else if (inputTwo === 0) {
+        results = 'ERROR';
     }
-    return results;
+    console.log(results)
 }
 
-const operatorAdd = add();
-const operatorSubtract = subtract();
-const operatorMultiply = multiply();
-const operatorDivide = divide();
 
-numbers.forEach(num => {
-    num.addEventListener('click', getInputOne);
-    num.addEventListener('click', getInputTwo);
-});
-
-operators.forEach(op => {
-    op.addEventListener('click', getOperator)
-})
-
-function getInputOne(e) {
-    if (!operator) {
-        inputOne = e.target.innerText;
-        display.textContent += inputOne;
-    }
-    console.log(inputOne);
-    return inputOne;
-}
-
-function getOperator(e) {
-    operator = e.target.innerText;
-    display.textContent = '';
-    console.log(operator)
-    return operator;
-}
-
-function getInputTwo(e) {
-    if (operator) {
-        inputTwo = e.target.innerText;
-        display.textContent += inputTwo;
-    }
-    console.log(inputTwo)
-    return inputTwo;
-}
-
-function operate() {
-    let operateVal;
-    if (inputOne) {
-        if (operator === '+') {
-            operateVal = operatorAdd;
-        } else if (operator === '-') {
-            operateVal = operatorSubtract;
-        } else if (operator === 'X') {
-            operateVal = operatorMultiply;
-        } else if (operator === '/') {
-            operateVal = operatorDivide;
-        } else if (operator === '=') {
-            operateVal = '=';
-            display.textContent = results;
-        }
-    } else {
-        operateVal = null;
-    }
-    return operateVal
-}
-operate(operator);
-
-// function getResult() {
-
-// }
-// getResult();
+add();
+subtract();
+multiply();
+divide();
