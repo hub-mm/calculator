a963868a6dc58836cda13cb59b2b0879d020d52c
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-07-10 14:07:39 +0700

    Display text functionality added.

--- script.js
+++ script.js
@@ -5,8 +5,22 @@ const display = document.querySelector('.display');
 let inputOne = parseFloat('');
 let inputTwo = parseFloat('');
 let arrInput = [inputOne, inputTwo,];
-let operator;
+let operator = '';
 let results;
+
+numbers.forEach(input => {
+    input.addEventListener('click', (e) => {
+        getInputOne(input, e)
+        getInputTwo(input, e)
+    })
+});
+
+
+operators.forEach(operatorText => {
+    operatorText.addEventListener('click', (e) => {
+        getOperator(operatorText, e);
+    })
+});
 
 function add() {
     results = arrInput.reduce((acc, val) => acc += val)
@@ -22,13 +36,30 @@ function multiply() {
 }
 function divide() {
     if (inputTwo !== 0) {
-    results = arrInput.reduce((acc, val) => acc /= val)
+        results = arrInput.reduce((acc, val) => acc /= val)
     } else if (inputTwo === 0) {
         results = 'ERROR';
     }
     console.log(results)
 }
 
+function getInputOne(input) {
+    inputOne = input.innerText;
+    display.textContent = inputOne;
+    console.log('inputOne: ', inputOne)
+}
+
+function getOperator(operatorText) {
+    operator = operatorText.innerText;
+    display.textContent = operator;
+    console.log('operator: ', operator)
+}
+
+function getInputTwo(input) {
+    inputTwo = input.innerText;
+    display.textContent = inputTwo;
+    console.log('inputTwo: ', inputTwo)
+}
 
 add();
 subtract();
