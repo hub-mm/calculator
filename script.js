319e65c1ec0d85c336705453fc990425af3f90b6
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-07-13 10:58:21 +0700

    Clean up code and rework method for cleaner and more efficient work.

--- script.js
+++ script.js
@@ -3,102 +3,158 @@ const operators = document.querySelectorAll('.operator');
 const displaySum = document.querySelector('.display-sum');
 const displayLive = document.querySelector('.display-live');
 
-let inputOne = parseFloat('');
-let inputTwo = parseFloat('');
-let arrInput = [inputOne, inputTwo,];
+let inputOne = '';
 let operator = '';
-let equals;
-let results;
+let inputTwo = '';
+let operatorEqual = '';
+let operatorFunc = '';
+let arrInput = [];
+let arrOp = [];
+let cal = '';
+let liveSum = ''
+let results = '';
 
-numbers.forEach(input => {
-    input.addEventListener('click', (e) => {
-        getInputOne(input, e)
-        getInputTwo(input, e)
+numbers.forEach(num => {
+    num.addEventListener('click', (e) => {
+        inputOne = getInputOne(num, e);
+        inputTwo = getInputTwo(num, e);
+        arrInput = getArrayNum(inputOne, inputTwo);
+        cal = getCal(arrInput, e);
+        liveSum = getLiveSum(inputOne, inputTwo);
+        console.log('inputOne: ', inputOne);
+        console.log('inputTwo: ', inputTwo);
+        console.log('arrInput: ', arrInput)
+        console.log('cal: ', cal);
+        console.log('liveSum: ', liveSum.textContent);
     })
 });
 
-operators.forEach(operatorText => {
-    operatorText.addEventListener('click', (event) => {
-        getOperator(operatorText, event);
-        getResult(operatorText, event);
+operators.forEach(opSelect => {
+    opSelect.addEventListener('click', (event) => {
+        operator = getOperator(opSelect, event);
+        operatorFunc = getOperatorFunc(operator, event);
+        operatorEqual = getOperatorEqual(opSelect, event);
+        arrOp = getArrayOp(operator, operatorEqual);
+        results = getResult(opSelect, event);
+        cal = getCal(arrOp, event);
+        liveSum = getLiveSum(results, event);
+        console.log('operator: ', operator)
+        console.log('operatorFunc: ', operatorFunc);
+        console.log('arrOP: ', arrOp);
+        console.log('results: ', results);
+        console.log('cal: ', cal);
+        console.log('liveSum: ', liveSum.textContent);
     })
 });
 
-function add() {
-    results = arrInput.reduce((acc, val) => acc += val)
+function add(arrInput) {
+    results = arrInput.reduce((acc, val) => acc += val);
     return results
 }
-function subtract() {
-    results = arrInput.reduce((acc, val) => acc -= val)
+function subtract(arrInput) {
+    results = arrInput.reduce((acc, val) => acc -= val);
     return results
 }
-function multiply() {
-    results = arrInput.reduce((acc, val) => acc *= val)
+function multiply(arrInput) {
+    results = arrInput.reduce((acc, val) => acc *= val);
     return results
 }
-function divide() {
+function divide(arrInput) {
     if (inputTwo !== 0) {
-        results = arrInput.reduce((acc, val) => acc /= val)
+        results = arrInput.reduce((acc, val) => acc /= val);
     } else if (inputTwo === 0) {
-        results = 'ERROR';
+        return 'ERROR'
     }
     return results
+
 }
 
-function getInputOne(input) {
+function getInputOne(num, e) {
     if (!operator) {
-        inputOne = input.innerText;
-        inputTwo = '';
-        displayLive.textContent += inputOne;
-        displaySum.textContent += inputOne;
-        console.log('inputOne: ', inputOne)
+        inputOne += e.target.innerText;
     }
     return inputOne
 }
 
-function getOperator(operatorText) {
-    if (inputOne) {
-        operator = operatorText.innerText;
-        // displayLive.textContent = operator;
-        displaySum.textContent += ` ${operator} `;
-        console.log('operator: ', operator)
+function getOperator(opSelect, event) {
+    if (inputOne !== '' && inputTwo === '') {
+        operator = event.target.innerText;
     }
     return operator
 }
 
-function getInputTwo(input) {
-    if (operator) {
-        if (!inputTwo) {
-            displayLive.textContent = '';
-        }
-        inputTwo = input.innerText;
-        displayLive.textContent += inputTwo;
-        displaySum.textContent += inputTwo;
-        console.log('inputTwo: ', inputTwo)
+function getInputTwo(num, e) {
+    if (operator !== '') {
+        inputTwo += e.target.innerText;
     }
     return inputTwo
 }
 
-function getResult(operatorText, results) {
-    if (operatorText.innerText === '=') {
-        displaySum.textContent += results;
+function getOperatorEqual(opSelect, event) {
+    if (event.target.innerText === '=') {
+        operatorEqual = event.target.innerText;
+    }
+    return operatorEqual
+}
+
+function getArrayNum(inputOne, inputTwo) {
+    if (inputOne !== '') {
+        arrInput = [parseFloat(inputOne), parseFloat(inputTwo)];
+    }
+    return arrInput
+}
+
+function getArrayOp(operator, operatorEqual) {
+    if (operator !== '') {
+        arrOp = [operator, operatorEqual];
+    }
+    return arrOp
+}
+
+function getOperatorFunc(operator) {
+    if (inputOne !== '' && operator !== '' && inputTwo !== '') {
+        if (operator === '+') {
+            operatorFunc = add(arrInput);
+        } else if (operator === '-') {
+            operatorFunc = subtract(arrInput);
+        } else if (operator === 'X') {
+            operatorFunc = multiply(arrInput);
+        } else if (operator === '/') {
+            operatorFunc = divide(arrInput);
+        }
+    }
+    return operatorFunc
+}
+
+function getResult(opSelect, event) {
+    if (event.target.innerText === '=') {
+        results = operatorFunc;
     }
     return results
 }
 
-function operate(operator) {
-    if (operator === '+') {
-        results = add(arrInput);
-        console.log(results)
-    } else if (operator === '-') {
-        results = subtract(arrInput);
-        console.log(results)
-    } else if (operator === 'X') {
-        results = multiply(arrInput);
-        console.log(results)
-    } else if (operator === '/') {
-        results = divide(arrInput);
-        console.log(results)
+function getLiveSum(event) {
+    liveSum = displayLive;
+    if (!operator) {
+        liveSum.textContent = inputOne;
+    } else if (inputTwo !== '' && results === '') {
+        liveSum.textContent = inputTwo;
+    } else if (results !== '') {
+        liveSum.textContent = results;
     }
-    return results
+    return liveSum
+}
+
+function getCal(e, event) {
+    cal = displaySum;
+    if (!operator) {
+        cal.textContent = inputOne;
+    } else if (operator !== '' && inputTwo === '') {
+        cal.textContent = `${arrInput[0]} ${arrOp[0]}`;
+    } else if (inputTwo !== '' && results === '') {
+        cal.textContent = `${arrInput[0]} ${arrOp[0]} ${arrInput[1]}`;
+    } else if (results !== '') {
+        cal.textContent = `${arrInput[0]} ${arrOp[0]} ${arrInput[1]} ${arrOp[1]} ${results}`;
+    }
+    return cal
 }
